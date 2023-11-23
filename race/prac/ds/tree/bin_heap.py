d204e31f48488c4ce33402f6e36839496f7af634
ShimShining <shinjolt@gmail.com>
2023-11-23 15:39:45 +0800

    优先队列-二叉堆

--- race/prac/ds/tree/bin_heap.py
+++ race/prac/ds/tree/bin_heap.py
@@ -11,3 +11,60 @@ class BinHeap:
         self.heap_list = [0]
         self.current_size = 0
 
+    def insert(self, key):
+        self.heap_list.append(key)
+        self.current_size += 1
+        self.perc_up(self.current_size)
+
+    def perc_up(self, i):
+        """
+        插入节点上浮
+        :param i:
+        :return:
+        """
+        while i // 2 > 0:
+            if self.heap_list[i] > self.heap_list[i // 2]:
+                self.heap_list[i], self.heap_list[i//2] = self.heap_list[i//2], self.heap_list[i]
+            i = i // 2
+
+    def del_min(self):
+        retval = self.heap_list[1]   # 移走堆顶
+        self.heap_list[1] = self.heap_list[self.current_size]
+        self.current_size = self.current_size - 1
+        self.heap_list.pop()
+        self.perc_down(1)
+        return retval
+
+    def perc_down(self, i):
+        while i * 2 <= self.current_size:
+            mc_index = self.min_child(i)
+            if self.heap_list[i] > self.heap_list[mc_index]:
+                self.heap_list[i], self.heap_list[mc_index] = self.heap_list[mc_index], self.heap_list[i]
+            i = mc_index  # 沿路径下沉
+
+    def min_child(self, i):
+        if i * 2 + 1 > self.current_size:
+            return i * 2   # 唯一子节点
+        # 返回较小的节点index
+        if self.heap_list[i * 2] < self.heap_list[i * 2 + 1]:
+            return i * 2
+        return i * 2 + 1
+
+    def build_heap(self, nums):
+        """
+        insert建堆 时间复杂度是nlogn
+        下沉法建堆 时间复杂度是n
+        :param nums:
+        :return:
+        """
+        i = len(nums) // 2
+        self.current_size = len(nums)
+        self.heap_list = [0] + nums[:]
+        print(len(self.heap_list), i)
+        while i > 0:
+            print(self.heap_list, i)
+            self.perc_down(i)
+            i = i - 1
+        print(self.heap_list, i)
+
+
