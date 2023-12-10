f7a4b13d16f433af2f1e1c808378b7148fa255f8
ShimShining <shinjolt@gmail.com>
2023-12-10 11:04:36 +0800

    hw-s-有效括号序列

---
+++ race/prac/algo/interview/hw/is_valid.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Author : shining
+Date: 2023/12/10
+Describe:NC52 有效括号序列
+给出一个仅包含字符'(',')','{','}','['和']',的字符串，判断给出的字符串是否是合法的括号序列
+括号必须以正确的顺序关闭，"()"和"()[]{}"都是合法的括号序列，但"(]"和"([)]"不合法。
+
+要求：空间复杂度 O(n)，时间复杂度 O(n)
+输入：
+"["
+返回值：
+false
+输入：
+"[]"
+返回值：
+true
+"""
+
+
+def is_valid(s):
+    res = []
+    n = len(s)
+    i = 0
+    while i < n:
+        if s[i] in "([{":
+            res.append(s[i])
+        else:
+            if not res:
+                return False
+            ch = res.pop()
+            if not match(ch, s[i]):
+                return False
+        i += 1
+    if res:
+        return False
+    return True
+
+
+def match(r, l):
+    pre = "([{"
+    post = ')]}'
+    return pre.index(r) == post.index(l)
+
+
+if __name__ == '__main__':
+    s = "()"
+    print(is_valid(s))
+
